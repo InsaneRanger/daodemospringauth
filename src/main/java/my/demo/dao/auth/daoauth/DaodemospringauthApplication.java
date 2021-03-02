3c7a7b13522114d737adae840098e2ced1dfbf84
Insane <valeev.timur.v@gmail.com>
2021-03-02 20:16:27 +0300

    First Commit properties for db/jwt

---
+++ src/main/java/my/demo/dao/auth/daoauth/DaodemospringauthApplication.java
@@ -0,0 +1,13 @@
+package my.demo.dao.auth.daoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaodemospringauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaodemospringauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/demo/dao/auth/daoauth/DaodemospringauthApplicationTests.java
@@ -0,0 +1,13 @@
+package my.demo.dao.auth.daoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaodemospringauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
